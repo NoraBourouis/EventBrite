0e5aec5f68a8d5a3c98a6228d50d856fd6089923
 NoraBourouis <bourouisnora@gmail.com>
2019-02-14 00:34:44 +0100

    peut etre mieux?

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     resources :participations, only: [:new, :index, :create]
   end
   resources :users, only: [:show, :edit, :update]
-  post "/events/:event_id/participations/:id", to: "participations#create", as: "event_participation"
-  delete "/events/:id/", to: "events#destroy", as: "destroy_event"
+
   root 'events#index'
 end
